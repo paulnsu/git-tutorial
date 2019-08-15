ad46568d87e415d7da17e779e53f273523c44c21
paulnsu <paulnsubd@gmail.com>
2019-08-15 18:34:30 +0600

    added a new display method to Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,12 @@ publi class Hello
     public static void main (String [] args)
     {
         System.out.println("VS code for first GitHub tutorial ....");
+        
+        display("paul");
+    }
+
+    public static void display (String name)
+    {
+        System.out.println("Welcome" + name);
     }
 }
